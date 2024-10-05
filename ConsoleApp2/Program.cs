02017d2a2ec6090c7c336f08c1224314d8fb24ef
Atigoodarzi <atiiehgoodarzi@gmail.com>
2024-10-05 15:44:40 +0330

    Added new features

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World2!");
+Console.WriteLine("Hello, World3!");
+Console.WriteLine("Hello, World4!");
+Console.WriteLine("Hello, World5!");
+Console.WriteLine("Hello, World6!");
+Console.WriteLine("Hello, World7!");
+Console.WriteLine("Hello, World8!");
+Console.WriteLine("Hello, World9!");
+
+
